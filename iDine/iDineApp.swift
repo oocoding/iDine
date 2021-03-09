2bc325dac5c57d549da3d81506aff89c1e8205fe
Zhang <zhang.xiangxiong@gmail.com>
2021-03-09 09:55:38 +0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Zhang.xiangxiong on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
