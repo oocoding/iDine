438152bfd96353dec1a11d7cec4e96dd3556fb07
Zhang <zhang.xiangxiong@gmail.com>
2021-03-10 11:10:30 +0800

    在 ForEach 中添加 OnDelete 实现手势删除。

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,86 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Zhang.xiangxiong on 2021/3/10.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    @State private var paymentType = "Credit Card"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var totalPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+
+        return formatter.string(from: NSNumber(value: total + tipValue)) ?? "$0"
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay", selection: self.$paymentType) {
+                    ForEach(paymentTypes, id:\.self) {
+                        Text($0)
+                    }
+                }
+                
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header:
+                Text("TOTAL: \(totalPrice)").font(.largeTitle)
+            ) {
+                VStack(alignment: .center) {
+                    Button(action:{
+                        self.showingPaymentAlert = true
+                    }) {
+                        Text("Confirm order")
+                            .foregroundColor(.red)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .alert(isPresented: $showingPaymentAlert) {
+                Alert(title: Text("Order confirmed"), message: Text("Your total was \(totalPrice) – thank you!"), dismissButton: .default(Text("OK")))
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CheckoutView()
+                .environmentObject(Order())
+        }
+    }
+}
--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -22,17 +22,27 @@ struct OrderView : View {
                             Text("$\(item.price)")
                         }
                     }
+                    .onDelete(perform: deleteItems)
                 }
 
                 Section {
-                    NavigationLink(destination: Text("Check out")) {
+                    NavigationLink(destination: CheckoutView()) {
                         Text("Place Order")
                     }
                 }
+                .disabled(order.items.isEmpty)
             }
             .navigationTitle("Order")
             .listStyle(InsetGroupedListStyle())
+            .toolbar {
+                EditButton()
+            }
         }
+    }
+    
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
